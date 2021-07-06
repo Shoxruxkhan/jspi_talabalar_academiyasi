ef601865d8e85febfbe750761cd9e3b6be21c0ad
Shoxrux Jumanov <shoxrux97jumanov@gmail.com>
2021-07-06 20:06:42 +0500

    herocy procfile qo'shildi

--- parse_fakulty.py
+++ parse_fakulty.py
@@ -25,7 +25,7 @@ db = mysql.connector.connect(
     )
 mycursor = db.cursor()
 mycursor.execute("CREATE TABLE IF NOT EXISTS accounts_fakulty (id INT(50) NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), dek VARCHAR(255))")
-mycursor.execute("ALTER TABLE academy1.accounts_fakulty CONVERT TO CHARACTER SET utf8")
+mycursor.execute("ALTER TABLE academy.accounts_fakulty CONVERT TO CHARACTER SET utf8")
 sql = "INSERT INTO accounts_fakulty (name, dek) VALUES (%s,%s)"
 mycursor.executemany(sql, product)
 db.commit()
